f7944d6914876478fc98e42419c804cd9e7bdf12
Simone Fisicaro <simonefisicaro@gmail.com>
2013-07-15 16:31:11 +0000

    Aggiunta lettura da file

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var infile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+    var buffer = new buffer(fs.readFile());
+    response.send(buffer.toString("utf-8", 0, 12));
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
